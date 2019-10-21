ec5966d0e338deac8242dc911a58e0433ecf93a4
Pier-Luc Nadeau <1732895@cegepba.qc.ca>
2019-10-21 17:02:53 -0400

    Red

---
+++ Cours16_shader/Assets/CameraEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class CameraEffect : MonoBehaviour {
+    private Material material;
+	// Use this for initialization
+    void Awake()
+    {
+        material = new Material(Shader.Find("Custom/ImageEffect"));
+    }
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination,material);
+    }
+}
